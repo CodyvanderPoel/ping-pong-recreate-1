8da0818d3687b49975306cf60471d7e9dd5eb1dd
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-11-02 15:39:11 -0500

    added initial page data; added an event to login the user (no confirmation shows to user yet)
    Additions/Changes:
    initialization: created variable PAGE_DATA that will hold data used through the webpage
    
    login:
    function that logs in the user via a fetch call to the login api; takes in a username and password that are both strings; fetch's optional request object uses the two parameters to build the json string to pass to the HTTP request body; first then handler returns a promise of the body; next then handler takes the body as a parameter and passes the token and the users api url through verifyUser and calls it
    
    verifyUser:
    takes in a token and a url; mainly involves a fetch call that takes the url parameter as its first argument, and a request object as its second argument that uses the token passed for authorization; tests the token to make sure it's a token or not; if it is, the token is set to PAGE_DATA.token property and fetch is called; the fetch uses the url parameter as well as the token to build the request; first then promises the body of the response; next then uses the object returned (which should be a list of registered users) and assigns it to PAGE_DATA.users
    
    addEvents:
    function that adds an event to the login form (might rename); gets the login form and assigns it to loginForm; uses this variable to get the username and password input elements (assigned to usernameInput, and passwordInput); adds an event listener to loginForm that listens for "submit" and the handler calls login with the value of the usernameInput and passwordInput

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    body: JSON.stringify({ username: "hello", password: "world" })
-})
-    .then(r => r.json())
-    .then(text => console.log(text));
+const PAGE_DATA = { token: null, users: null };
+
+function login(username, password) {
+    fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        body: JSON.stringify({ username: username, password: password })
+    })
+        .then(r => r.json())
+        .then(text =>
+            verifyUser(
+                text.token,
+                "https://bcca-pingpong.herokuapp.com/api/users/"
+            )
+        );
+}
+function verifyUser(token, url) {
+    if (token) {
+        PAGE_DATA.token = token;
+        fetch(url, {
+            method: "GET",
+            headers: {
+                // "Content-Type": "application/json; charset=utf-8",
+                Authorization: "Token " + token
+            }
+        })
+            .then(r => r.json())
+            .then(text => (PAGE_DATA.users = text));
+    }
+}
+
+function addEvents() {
+    loginForm = document.getElementById("login-form");
+    usernameInput = loginForm["username"];
+    passwordInput = loginForm["password"];
+    loginForm.addEventListener("submit", ev => {
+        ev.preventDefault();
+        login(usernameInput.value, passwordInput.value);
+    });
+}
+addEvents();
